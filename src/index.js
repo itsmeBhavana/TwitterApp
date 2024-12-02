4fb61317cdab4c536eea6cdb07463797cec10c64
itsmeBhavana <bhavana.matavalam@gmail.com>
2024-12-01 22:12:06 -0600

    Added ejs templating engine

--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
 import express from "express";
 import morgan from "morgan";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+
 app.use(morgan("combined"));
+
+app.use(express.json());
+app.use(express.text());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.render("home", { name: "Bhavana Matavalam" });
+});
 
 app.get("/ping", (req, res) => {
   res.json({
